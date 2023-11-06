5582b305e81f5239d5b385486481591166004e25
Nati <natipo1221@gmail.com>
2023-11-06 15:56:15 +0200

    username-search-server

--- routers/routerAuth.js
+++ routers/routerAuth.js
@@ -139,4 +139,24 @@ router.post(
   }
 );
 
+router.post('/username', async (req, res) => {
+  // get the userName.
+  const userName = req.body.Name;
+  // check if the username is in the DB.
+  try {
+    let user = await getOneUser({ username: userName });
+    console.log(user);
+    if(!user){
+      return res.status(200).json({msg: "Not exist"})
+    }
+    else{
+      return res.status(400).json({msg: "exist"})
+    }
+  } catch (error) {
+    return res.status(200).json({msg: "Not exist"})
+  }
+
+
+})
+
 export default router;
