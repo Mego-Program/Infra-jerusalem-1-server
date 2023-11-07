4e03e80b99cf818ab114b80ea1c5326225a2a171
Moshe Brizel <139878655+MosheBrizel@users.noreply.github.com>
2023-11-07 23:14:05 +0200

    Update dbCoonect.js
    change to versel

--- db/dbCoonect.js
+++ db/dbCoonect.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    await mongoose.connect(process.env.MONGODB_URI
+, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
