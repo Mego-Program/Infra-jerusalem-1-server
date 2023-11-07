3bfb6bb8ed389a49e9919ef7432550fbac0e3c6a
Moshe Brizel <brizelmoshe@gmail.com>
2023-11-07 20:48:05 +0200

    change the forget password router

--- routers/routerForgetPassword.js
+++ routers/routerForgetPassword.js
@@ -2,7 +2,8 @@ import { Router } from "express";
 import sendEmail from "../middleware/sendEmailToTheClient.js";
 import { getOneUser, updeteOneUser } from "../db/functionToDB.js";
 import calculateDateDifference from "../functins/calculateDateDifference.js";
-import bcrypt from 'bcrypt'
+import bcrypt from "bcrypt";
+import randomPassword from "../functins/randomPassword.js";
 
 const router = Router();
 
@@ -20,9 +21,9 @@ router.post("/email", async (req, res) => {
   // try to update the code and send it to the email.
   try {
     //creat a rundom code.
-    const verifyCode = Math.floor(Math.random() * 90000) + 10000;
+    const verifyPassword = randomPassword();
     // send a email to the cloent.
-    const reqEmail = await sendEmail(email, verifyCode);
+    const reqEmail = await sendEmail(email, verifyPassword);
     // if the is not send a email send error.
     if (!reqEmail) {
       return res.status(400).json({
@@ -31,7 +32,7 @@ router.post("/email", async (req, res) => {
     }
     // save the changes.
     const resSave = await updeteOneUser(email, {
-      "verifyEmail.value": verifyCode,
+      "verifyEmail.value": verifyPassword,
       "verifyEmail.date": new Date(),
     });
     console.log(resSave);
@@ -53,77 +54,46 @@ router.post("/email", async (req, res) => {
   }
 });
 
-router.post("/code", async (req, res) => {
+router.post("/password", async (req, res) => {
   // get the code and the email
-  const { email, code } = req.body;
+  const { email, password } = req.body;
   // check if the code is corect.
-  const user = await getOneUser({ email: email, "verifyEmail.value": code});
+  const user = await getOneUser({
+    email: email,
+    "verifyEmail.value": password,
+  });
   // if is not foud send error.
   if (!user) {
     return res.status(400).json({
       mag: "not a corect code",
     });
   }
-  const diffTime = calculateDateDifference(new Date(user.verifyEmail.date) , new Date())
+  const diffTime = calculateDateDifference(
+    new Date(user.verifyEmail.date),
+    new Date()
+  );
   console.log(diffTime);
-  if (diffTime.hours != 0 || diffTime.minutes > 2){
+  if (diffTime.hours > 0 || diffTime.minutes > 5) {
     return res.status(400).json({
-      mag: "time of the code is over",
-    });
-  }
-  try {
-    // creat a code.
-    const verifyCodeClient = Math.floor(Math.random() * 90000) + 10000;
-    // update the code of the user
-    const saveUser = await updeteOneUser(email, {"verifyEmail.value" : verifyCodeClient,"verifyEmail.date":new Date()})
-    if (!saveUser) {
-      return res.status(400).json({
-        mag: "error in DB",
-      });
-    }
-    // Send OK to the client with the code.
-    return res.status(200).json({
-      code: verifyCodeClient,
-    });
-  } catch (error) {
-    return res.status(400).json({
-      mag: error,
-    });
-  }
-});
-
-router.post("/changePassword", async (req, res) => {
-  // Get the email, password, code.
-  const { email, password, code } = req.body;
-  // check if the code is corect.
-  const user = await getOneUser({ email: email, "verifyEmail.value": code });
-  // If is not find a user send a error
-  if (!user) {
-    return res.status(400).json({
-      mag: "user is not found",
-    });
-  }
-  const diffTime = calculateDateDifference(new Date(user.verifyEmail.date) , new Date())
-  console.log(diffTime);
-  if (diffTime.hours != 0 || diffTime.minutes > 10){
-    return res.status(400).json({
-      mag: "time of the code is over",
+      mag: "time of the password is over",
     });
   }
   try {
     // update the DB.
-    const saveDB = await updeteOneUser(email,{password : await bcrypt.hash(password, 10)})
+    const saveDB = await updeteOneUser(email, {
+      password: await bcrypt.hash(password, 10),
+    });
     if (!saveDB) {
       return res.status(400).json({
         mag: "error in DB",
       });
+    } else {
+      // Send OK to the client with the code.
+      return res.status(200).json({
+        code: "change the password",
+      });
     }
-
-    return res.status(200).json({
-      msg: "the password is changed",
-    });
   } catch (error) {
-    // send the error.
     return res.status(400).json({
       mag: error,
     });
